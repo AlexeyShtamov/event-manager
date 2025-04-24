32ef93a0b18e7ecdb099fd440249d492ccd08acb
Alexey <alexshamox@gmail.com>
2025-04-24 11:17:14 +0500

    first commit

---
+++ src/main/java/ru/shtamov/eventmanaget/EventManagetApplication.java
@@ -0,0 +1,13 @@
+package ru.shtamov.eventmanaget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/shtamov/eventmanaget/EventManagetApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shtamov.eventmanaget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
